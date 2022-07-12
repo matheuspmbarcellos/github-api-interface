7ac37b1830ade950d8451f06e29f36ce8ce3b926
Matheus Barcellos <matbarcellos21@gmail.com>
2022-07-11 23:38:23 -0300

    Create github-hooks.js

---
+++ github-api/src/hooks/github-hooks.js
@@ -0,0 +1,12 @@
+import { useContext } from "react";
+import { GithubContext } from "../providers/github-provider";
+
+
+const useGithub = () => {
+
+    const { githubState } = useContext(GithubContext);
+
+    return { githubState };
+};
+
+export default useGithub;
